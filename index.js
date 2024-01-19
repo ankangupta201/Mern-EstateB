e8b1205f3ee36e40c0f5e871d80bdaafc3b2582b
Ankan Gupta <ankan.gupta@vyrazu.com>
2024-01-19 18:31:36 +0530

    mongo connected

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 require('dotenv').config()
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.URL).then(()=>{
+  console.log("connected to mongodb")
+}).catch((err)=>{
+  console.log(err)
+})
 
 const app = express()
 
