375cb240ba342b221b6d7247e70acde68936c225
chiranjibJha <chiranjibjha1999@gmail.com>
2024-01-21 21:20:19 +0530

    auth and login added

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,6 +1,8 @@
 const { User } = require("../models/db");
 const { z } = require("zod");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const Auth = {};
 Auth.register = async function register(req, res, next) {
   try {
@@ -27,13 +29,69 @@ Auth.register = async function register(req, res, next) {
       email: data.email,
       password: hashPassword,
     });
+    const signedToken = generateToken(user);
 
-    res
-      .status(201)
-      .json({ status: true, message: "User created Successfully", user: user });
+    const userWithoutPassword = { ...user.toObject() };
+    delete userWithoutPassword.password;
+
+    res.status(201).json({
+      status: true,
+      message: "User created Successfully",
+      user: userWithoutPassword,
+      token: signedToken,
+    });
   } catch (error) {
     next(error);
   }
 };
+
+Auth.login = async function (req, res, next) {
+  try {
+    console.log(req.body);
+    const data = req.body;
+    const loginSchema = z.object({
+      email: z.string().email(),
+      password: z.string(),
+    });
+    loginSchema.parse(data);
+
+    const user = await User.findOne({ email: data.email });
+    if (!user) {
+      res.status(401).json({ status: false, message: "User Not Found" });
+    }
+    const result = await bcrypt.compare(data.password, user.password);
+    if (result) {
+      const signedToken = generateToken(user);
+      const userWithoutPassword = { ...user.toObject() };
+      delete userWithoutPassword.password;
+      return res.status(201).json({
+        status: true,
+        message: "User loged in successfully",
+        user: userWithoutPassword,
+        token: signedToken,
+      });
+    } else {
+      res
+        .status(401)
+        .json({ status: false, message: "Email or password is wrong" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+function generateToken(user) {
+  const id = user._id;
+  const expiresIn = "1d";
+  const payload = {
+    sub: id,
+    iat: Date.now(),
+  };
+  const signedToken = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: expiresIn,
+    algorithm: "HS256",
+  });
+  return "Bearer " + signedToken;
+}
 
 module.exports = Auth;
--- src/middlewares/GlobalError.js
+++ src/middlewares/GlobalError.js
@@ -1,9 +1,11 @@
 const globalErrorHandler = (err, req, res, next) => {
   console.error(err);
   if (err.name === "Error") {
-    return res.status(400).json({ error: "error occurred" });
+    return res.status(400).json({ error: "error occurred " + err });
   }
-  res.status(500).json({ status: false, error: "Internal Server Error" + err });
+  res
+    .status(500)
+    .json({ status: false, error: "Internal Server Error " + err });
 };
 
 module.exports = {
